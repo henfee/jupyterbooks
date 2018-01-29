d6dc6d0a2bd86259c2683b8efa24b74c8574da2a
Xubin Cao <caoxubin@gmail.com>
2018-01-29 23:42:19 +0800

    add pylint to check the code

--- 00LearningJupyter/codes/baidu_trans.py
+++ 00LearningJupyter/codes/baidu_trans.py
@@ -1,40 +1,42 @@
 #/usr/bin/env python
 #coding=utf8
- 
+
 #import httplib  #python2
 import http.client #python3
 #import md5 #python2
 import hashlib
 import urllib.request as ur
 import random
-import requests
 import json
+import requests
 
 
-# use baidu tranlslate api 
+# use baidu tranlslate api
 def baidu_translate(texts):
-    ''' use baidu tranlate api to translate English texts into Chinese, return list[src, dst], you can use return[0] or return[1] to choose'''
+    ''' use baidu tranlate api to translate English texts into Chinese,
+     return list[src, dst], you can use return[0] or return[1] to choose'''
+
     appid = '20180104000112180'
     secretKey = 'aJNKIfe1rn6Cf4vSKUJl'
     q = texts
-    if q =="":
+    if q == "":
         q = "<br>"
     httpClient = None
     myurl = '/api/trans/vip/translate'
     fromLang = 'en'
     toLang = 'zh'
     salt = random.randint(32768, 65536)
-    
+
     sign = appid+q+str(salt)+secretKey
     m1 = hashlib.md5()
     m1.update(sign.encode(encoding="utf-8", errors='strict'))
     sign = m1.hexdigest()
-    myurl = myurl+'?appid='+appid+'&q='+ur.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
-    
+    myurls = myurl+'?appid='+appid+'&q='+ur.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+
     try:
         httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
-        httpClient.request('GET', myurl)
- 
+        httpClient.request('GET', myurls)
+
         #response是HTTPResponse对象
         response = httpClient.getresponse()
         trans_result = response.read().decode('unicode-escape') # 注意转化unicode为UTF-8，结果为json
@@ -42,18 +44,18 @@ def baidu_translate(texts):
         src = json.loads(trans_result)['trans_result'][0]['src']
         dst = json.loads(trans_result)['trans_result'][0]['dst']
         #print(src,dst)
-        return src,dst
+        return src, dst
     except Exception as e: # 如果为空，报错，返回空字符给函数返回值
         print(e)
         src = ""
         dst = ""
-        return src,dst 
+        return src, dst
     finally:
         if httpClient:
             httpClient.close()
 
-src=baidu_translate("apple egg computer")[0]
-dst=baidu_translate("apple egg computer")[1]
+#src = baidu_translate("apple egg computer")[0]
+#dst = baidu_translate("apple egg computer")[1]
 
-print(src)
-
+if __name__ == '__main__':
+    baidu_translate()
